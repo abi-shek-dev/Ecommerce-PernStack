14d7033361e16b4bff1d9045c4b12d35dbd42bff
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-12-27 22:45:00 +0530

    update and delete user function for inngest

--- gocart/inngest/functions.ts
+++ gocart/inngest/functions.ts
@@ -18,3 +18,34 @@ export const syncUserCreation = inngest.createFunction(
         })
     }
 )
+
+// Function to update user data in database
+
+export const syncUserUpdation = inngest.createFunction(
+    {id: 'sync-user-update'},
+    {event: 'clerk/user.updated'},
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.delete({
+            where : { id: data.id }
+        })
+    }
+)
+
+// Function to delete user data from database
+
+export const syncUserDeletion = inngest.createFunction(
+    {id: 'sync-user-delete'},
+    {event: 'clerk/user.deleted'},
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.update({
+            where : { id: data.id },
+            data: {
+                email: data.email_addresses[0].email_addresses,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+        })
+    }
+)
